fc6f9f5b37ee14d1f28039d5b51055a94d34e4a1
John Grubb <therealjohnnygrubb@gmail.com>
2013-04-07 11:40:03 +0000

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hearts::Application.config.secret_token = 'b82a213aa4c6579dfa8b1b379d4e0c367d94108753b4ecf5cee55be78fb2e6688154671a178352f6c9706f61dea060ae123c9998948293945482ecdfa0973cd1'
