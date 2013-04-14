461f461ba50655584034a10df6ea8427752b9732
John Grubb <therealjohnnygrubb@gmail.com>
2013-04-14 15:28:47 -0400

    prevent default

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -2,11 +2,12 @@
 // All this logic will automatically be available in application.js.
 
 jQuery(function(){
-  $('.song a').click(function(){
+  $('.song a').click(function(e){
     var player = document.getElementById('player');
     player.src = $(this).data('source');
     player.controls = true;
     player.autoplay = true;
     player.style.width = "100%";
+    e.preventDefault();
   });
 });
