d287517e4ecc7765ad1f2ed0b378fff727a3d405
John Grubb <therealjohnnygrubb@gmail.com>
2013-04-25 11:10:24 -0400

    mobile nav

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+jQuery(function($){
+  $('a.btn-navbar').bind('click', function(){
+    var nav = $('nav#main');
+    console.log(nav.height());
+    if(nav.hasClass('visible')) {
+      nav.slideUp(150).removeClass('visible');
+    } else {
+      nav.slideDown(150).addClass('visible');
+    }
+  });
+});
