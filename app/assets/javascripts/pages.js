0a93e6f64081584dad0b193bc7c73c5145908552
John Grubb <therealjohnnygrubb@gmail.com>
2013-07-25 01:45:57 +0000

    MOAR CHANGES

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -15,4 +15,18 @@ jQuery(function($){
   $('a.dropdown-toggle').bind('click', function() {
    $(this).next('.dropdown-menu').css('display', 'block');
   });
+  $('html.home #logo h1').css({
+    'position' : 'absolute',
+    'left' : function() {
+      var winWidth = window.document.width;
+      var logoWidth = $('#logo h1').width();
+      return (winWidth - logoWidth) / 2;
+    },
+    'top' : function() {
+      var winHeight = window.document.height;
+      var logoHeight = $('#logo h1').height();
+      return (winHeight - logoHeight) / 2;
+    },
+    'margin' : 0
+  }).unwrap();
 });
