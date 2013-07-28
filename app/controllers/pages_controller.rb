162879e2d23d76248ca21442f7ee33120c55aa87
John Grubb <therealjohnnygrubb@gmail.com>
2013-07-28 07:31:23 -0400

    Page titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,23 +6,28 @@ class PagesController < ApplicationController
   def shows
     @html_class = "shows"
     @shows = Show.where('date > ?', 1.day.ago).order("date ASC")
+    @title = "Tour Dates"
   end
   
   def music
     @html_class = "shows"
     @songs = Song.all
+    @title = "Music"
   end
   
   def photos
     @html_class = "band"
     @photos = Photo.all
+    @title = "Media"
   end
   
   def band
     @html_class = "band"
+    @title = "The Band"
   end
   
   def contact
     @html_class = "band"
+    @title = "Contact"
   end
 end
