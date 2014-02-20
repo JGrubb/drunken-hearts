5524e5db5266713efe0082b413dbbcf45bd4b335
John Grubb <therealjohnnygrubb@gmail.com>
2014-02-20 14:32:42 -0500

    show model specs

--- spec/factories/shows.rb
+++ spec/factories/shows.rb
@@ -2,5 +2,12 @@
 
 FactoryGirl.define do
   factory :show do
+    date Date.today
+    venue "the Fillmore"
+    city "Denver, CO"
+    factory :show_w_links do
+      venue_link "http://www.ignoredbydinosaurs.com"
+      tickets_link "http://www.thedrunkenhearts.com"
+    end
   end
 end
--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -1,5 +1,46 @@
 require 'spec_helper'
 
 describe Show do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with a date, venue, and city" do
+    show = build :show
+    expect(show).to be_valid
+  end
+
+  it "is invalid without a date" do
+    show = build(:show, date: nil)
+    expect(show).to have(1).error_on(:date)
+  end
+
+  it "is invalid without a venue" do
+    show = build(:show, venue: nil)
+    expect(show).to have(1).error_on(:venue)
+  end
+
+  it "is invalid without a city" do
+    show = build(:show, city: nil)
+    expect(show).to have(1).error_on(:city)
+  end
+
+  describe "URL Validations" do
+    it "is valid with a legit ticket URL" do
+      show = build(:show_w_links)
+      expect(show).to be_valid
+    end
+
+    it "is invalid with a busted ticket link" do
+      show = build(:show, tickets_link: "ignoredbydinosaurs.com")
+      expect(show).to have(1).error_on(:tickets_link)
+    end
+
+    it "is valid with a legit venue URL" do
+      show = build(:show_w_links)
+      expect(show).to be_valid
+    end
+
+    it "is invalid with a busted venue link" do
+      show = build(:show, venue_link: "ignoredbydinosaurs.com")
+      expect(show).to have(1).error_on(:venue_link)
+    end
+  end
 end
