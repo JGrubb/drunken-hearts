74b7f3a980dc1d19a33f324426440f6d2a20fca0
John Grubb <therealjohnnygrubb@gmail.com>
2014-02-25 16:57:53 -0500

    begin product model

---
+++ db/migrate/20140225215709_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :download
+      t.text :description
+      t.integer :price
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
