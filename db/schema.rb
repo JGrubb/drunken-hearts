0e1c077d37b08c3cc54146e8f0e96960c9a7b301
John Grubb <therealjohnnygrubb@gmail.com>
2014-02-25 17:04:52 -0500

    adding product image model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225215709) do
+ActiveRecord::Schema.define(version: 20140225220017) do
 
   create_table "photos", force: true do |t|
     t.string   "caption"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20140225215709) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "product_images", force: true do |t|
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "product_images", ["product_id"], name: "index_product_images_on_product_id"
 
   create_table "products", force: true do |t|
     t.string   "title"
